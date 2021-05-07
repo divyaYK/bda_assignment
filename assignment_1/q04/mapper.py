2b6613c5c789438456f81d57c683f94aaa273774
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-08 04:37:30 +0530

    third attempt and other problems

--- assignment_1/q04/mapper.py
+++ assignment_1/q04/mapper.py
@@ -5,19 +5,44 @@ import sys
 
 inputFile = open(sys.argv[1], 'r')
 
-# To decrease computations
-prime_numbers = [2, 3, 5, 7]
+
+prime_numbers = []
+numbers = []
+numbers_to_send = []
 
 for line in inputFile:
-  number = int(line)
-  isPrime = True
-  if number > 1:
-    for i in prime_numbers:
-       if (number % i) == 0:
-         isPrime = False
+  numbers.append(int(line))
 
+max_number = max(numbers)
+numbers.remove(1)
+numbers.remove(0)
+
+isPrime = True
+for i in range(2, max_number+1):
+    for j in range(2, i):
+      if i%j==0:
+        isPrime = False
     if isPrime:
-      prime_numbers.append(number)
-      print(number)
+      prime_numbers.append(i)
+    isPrime = True
+
+for number in numbers:
+  for prime_number in prime_numbers:
+    if prime_number >= number:
+      break
+    elif number%prime_number == 0:
+      isPrime = False
+
+  if isPrime:    
+    if number in numbers_to_send:
+      continue
+    else:
+      numbers_to_send.append(number)
+  else:
+    isPrime = True
+
+
+for number in numbers_to_send:
+  print(number)
 
 inputFile.close()
