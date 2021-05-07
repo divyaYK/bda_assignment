a3e9a4d75a0db70866c34b8085d8eb3b5009251a
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-08 03:53:21 +0530

    Third attempt and other problems

--- assignment_1/q04/mapper.py
+++ assignment_1/q04/mapper.py
@@ -5,15 +5,19 @@ import sys
 
 inputFile = open(sys.argv[1], 'r')
 
+# To decrease computations
+prime_numbers = [2, 3, 5, 7]
+
 for line in inputFile:
   number = int(line)
   isPrime = True
   if number > 1:
-    for i in range(2, number):
+    for i in prime_numbers:
        if (number % i) == 0:
          isPrime = False
 
     if isPrime:
+      prime_numbers.append(number)
       print(number)
 
 inputFile.close()
