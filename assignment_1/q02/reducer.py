574a7c94f17d56d4d94f832714907da96fda8fd3
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-14 02:02:15 +0530

    q02 modification

--- assignment_1/q02/reducer.py
+++ assignment_1/q02/reducer.py
@@ -5,11 +5,13 @@ import sys
 
 numerator = 2
 denominator = 1
+instances_count = 0
 
 for line in sys.stdin:
   denominator *= round(float(line), 5)
   denominator = round(denominator, 5)
+  instances_count += 1
 
 pi = numerator/denominator
 
-print(pi)
+print(pi/instances_count)
