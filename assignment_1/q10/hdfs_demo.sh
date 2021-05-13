2292d7d3093fb134da3c759970c6c935fe912638
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-14 03:27:26 +0530

    q10 complete

--- assignment_1/q10/hdfs_demo.sh
+++ assignment_1/q10/hdfs_demo.sh
@@ -5,5 +5,5 @@ cd ..
 hdfs dfs -mkdir assign1/q10
 hdfs dfs -mkdir assign1/q10/input
 hdfs dfs -copyFromLocal input/* assign1/q10/input
-hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -D mapreduce.jobs.maps=2 -file ./mapper.py -mapper "python ./mapper.py 10000" -file ./reducer.py -reducer ./reducer.py -input /user/maria_dev/assign1/q10/input/ -output assign1/q10/final_output
+hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -D mapreduce.jobs.maps=4 -file ./mapper.py -mapper "python ./mapper.py 25 200 500" -file ./reducer.py -reducer ./reducer.py -input /user/maria_dev/assign1/q10/input/ -output assign1/q10/final_output
 hdfs dfs -cat assign1/q10/final_output/part-00000
