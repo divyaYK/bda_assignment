c1abc46b595ad4ea992d3d214b214376698d2607
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-14 18:05:33 +0530

    q11 & q12 complete

--- assignment_1/q11/generate_input.py
+++ assignment_1/q11/generate_input.py
@@ -6,7 +6,6 @@ max_number = int(sys.argv[2])
 file_name = sys.argv[3]
 
 inputFile = open(file_name, 'w')
-random.seed(15)
 
 for i in range(number_of_input_entries):
   inputFile.write(str(random.randint(0, max_number))+"\n")
