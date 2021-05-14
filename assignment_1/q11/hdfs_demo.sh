03b3db6505a526bb2ae81a625e59cf827f263639
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-14 17:06:06 +0530

    q11 complete

--- assignment_1/q11/hdfs_demo.sh
+++ assignment_1/q11/hdfs_demo.sh
@@ -5,5 +5,5 @@ cd ..
 hdfs dfs -mkdir assign1/q11
 hdfs dfs -mkdir assign1/q11/input
 hdfs dfs -copyFromLocal input/* assign1/q11/input
-hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -D mapreduce.jobs.maps=4 -file ./mapper.py -mapper "python ./mapper.py ./input.txt" -file ./reducer.py -reducer ./reducer.py -input /user/maria_dev/assign1/q11/input/ -output assign1/q11/final_output
+hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar -D mapreduce.jobs.maps=4 -file ./mapper.py -mapper "python ./mapper.py" -file ./reducer.py -reducer ./reducer.py -input /user/maria_dev/assign1/q11/input/ -output assign1/q11/final_output
 hdfs dfs -cat assign1/q11/final_output/part-00000
