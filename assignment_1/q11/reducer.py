85d3e7f0c785c9d33fcb2dcd43c95e7a02c46acd
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-15 12:19:20 +0530

    q11 & q12 complete

--- assignment_1/q11/reducer.py
+++ assignment_1/q11/reducer.py
@@ -11,29 +11,29 @@ min_list = []
 
 # input comes from STDIN
 for line in sys.stdin:
-    if line!="":
-      number, occurrence = line.split()
-      number = int(number)
-      occurrence = int(occurrence)
+    if line != "":
+        number, occurrence = line.split()
+        number = int(number)
+        occurrence = int(occurrence)
 
-      if number in number_n_occurrences.keys():
-        number_n_occurrences[number] += occurrence
-      else: 
-        number_n_occurrences[number] = occurrence
-      
+        if number in number_n_occurrences.keys():
+            number_n_occurrences[number] += occurrence
+        else:
+            number_n_occurrences[number] = occurrence
+
 max_occurrences = max(number_n_occurrences.values())
 min_occurrences = min(number_n_occurrences.values())
 
 for key, value in number_n_occurrences.items():
-  if value == max_occurrences:
-    max_list.append(key)
-  if value == min_occurrences:
-    min_list.append(key)
+    if value == max_occurrences:
+        print("Max occurred: ", key, value)
+    if value == min_occurrences:
+        print("Min occurred: ", key, value)
 
-print("Maximum numbers and their number of occurrences")
-for number in max_list:
-  print(number, max_occurrences)
+# print("Maximum numbers and their number of occurrences")
+# for number in max_list:
+#     print(number, max_occurrences)
 
-print("Minimum numbers and their number of occurrences")
-for number in min_list:
-  print(number, min_occurrences)
+# print("Minimum numbers and their number of occurrences")
+# for number in min_list:
+#     print(number, min_occurrences)
