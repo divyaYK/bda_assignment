685c5decbd79e3d2cd05f1e7c008ec0bdc262594
Divya Yashaswi Kanney <disney.kstew@gmail.com>
2021-05-15 12:33:42 +0530

    q11 & q12 complete

--- assignment_1/q11/reducer.py
+++ assignment_1/q11/reducer.py
@@ -22,10 +22,6 @@ for line in sys.stdin:
 max_occurrences = max(number_n_occurrences.values())
 min_occurrences = min(number_n_occurrences.values())
 
-print("max occurrences: ", max_occurrences)
-print("min occurrences: ", min_occurrences)
-print("dict: ", number_n_occurrences)
-
 for key, value in number_n_occurrences.items():
     if value == max_occurrences:
         print("Max occurred: ", key, value)
